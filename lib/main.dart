82fe121f7d712f17dc3148cb5021ef3f9c922895
AlejandroMoreira <Alejandromoreira1996@gmail.com>
2020-09-23 18:05:52 +0200

    cambio radical

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,28 @@
-import 'package:alejandroflutterapp4/providers/products_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'screens/products_details_screen.dart';
+import 'providers/products.dart';
+import 'screens/product_detail_screen.dart';
 import 'screens/products_overview_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ProductsProvider(),
-      //Sólo se rebuildearan los widget que hagan listener a esto
+    return ChangeNotifierProvider.value(
+      value: Products(),
       child: MaterialApp(
-        title: "",
-        theme: ThemeData(
-          primarySwatch: Colors.lightBlue,
-          accentColor: Colors.lightBlueAccent,
-          buttonColor: Colors.red,
-          canvasColor: Color.fromRGBO(255, 255, 234, 1),
-          fontFamily: "Laton",
-        ),
-        home: ProductsOverviewScreen(),
-        routes: {
-          "": (_) => ProductsOverviewScreen(),
-          "/product-detail": (_) => ProductsDetailsScreen(),
-        },
-      ),
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          }),
     );
   }
 }
